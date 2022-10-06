e62e449da70d72cfe92adc0f0efc947c04633555
qwjyh <urataw421@gmail.com>
2022-10-07 04:02:07 +0900

    add: fish: config.fish to resolve julia driver problem

--- dotfiles/fish/config.fish
+++ dotfiles/fish/config.fish
@@ -7,3 +7,8 @@ end
 
 # opam configuration
 source /home/qwjyh/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
+
+# julia
+# to solve Graphic Card driver problem
+export LD_PRELOAD=/usr/lib64/libstdc++.so.6
+
