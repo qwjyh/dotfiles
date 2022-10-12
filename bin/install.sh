ef2d9a239d70b0d711020cb451870bf112253e2a
qwjyh <urataw421@gmail.com>
2022-10-12 10:26:59 +0900

    update: sh: install script

--- bin/install.sh
+++ bin/install.sh
@@ -1,1 +1,22 @@
-ln -s (pwd)/dotfiles/fish/config.fish ~/.config/fish/config.fish
+#!/usr/bin/bash
+set -eu
+
+# =========================================================
+# check executing location
+# =========================================================
+if [ ! -d "bin" -a ! -d "dotfiles" ]; then
+	echo "Wrong execution location"
+	echo "Please run this script on dotfiles root folder."
+	exit 1
+fi
+
+
+# =========================================================
+# links
+# =========================================================
+ln -sf (pwd)/dotfiles/fish/config.fish ~/.config/fish/config.fish
+ln -sf (pwd)/dotfiles/starship/starship.toml ~/.config/starship.toml
+ln -sf (pwd)/dotfiles/tmux.conf ~/.tmux.conf
+
+
+ln -sf (pwd)/dotfiles/nvim/init.lua ~/.config/nvim/init.lua
