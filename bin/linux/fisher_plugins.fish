f7232b229bc4bd4d0fb6d2587ad5a8b64445cf9f
qwjyh <urataw421@gmail.com>
2022-10-21 17:30:24 +0900

    add: fish: auto fisher install

--- bin/linux/fisher_plugins.fish
+++ bin/linux/fisher_plugins.fish
@@ -7,8 +7,17 @@
 # check fisher existence
 if ! type fisher &>/dev/null
 	echo "fisher does not exist."
-	echo "Please install fisher."
-	return 1
+	echo "Please install fisher from https://github.com/jorgebucaran/fisher"
+	echo ""
+	echo "==Automatic install=="
+	echo "You can run `curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher` right here"
+	echo "be sure to check install command on the fisher project homepage."
+	read -p "echo Install\?(default Y / N):\ " ans
+	if [ $ans = 'N' ]
+		return 1
+	end
+	# install script from github page
+	curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
 end
 
 fisher update
