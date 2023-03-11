b8fff8602e3a552c587ef22d101552040ea34ced
qwjyh <urataw421@gmail.com>
2023-03-11 22:53:31 +0900

    add: nvim: LS keybindings & Lua LS
    - keybindings from `lsp-config`
    - Lua LS config from `lsp-config`

--- dotfiles/neovim/init.lua
+++ dotfiles/neovim/init.lua
@@ -44,7 +44,76 @@ end
 -----------------------------------------------------------
 -- Plugins
 require 'plugins'
-vim.cmd [[autocmd BufWritePost plugins.lua PackerCompile]]
+--vim.cmd [[autocmd BufWritePost plugins.lua PackerCompile]]
+
+-----------------------------------------------------------
+-- LSP config
+
+-- Mapping for language server
+-- See `:help vim.diagnostic.* for documentation on any of the below functions
+local opts = { noremap = true, silent = true }
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
+
+-- Use an on_attach function to only map the following keys
+-- after the language server attaches to the current buffer
+local on_attach = function(client, bufnr)
+    -- Enable completion triggered by <c-x><c-o>
+    vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+    -- Mappings
+    -- See `:help vim.lsp.*` for documentation on any of the below function
+    local bufopts = { noremap = true, silent = true, buffer = bufnr }
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+    vim.keymap.set('n', 'g1', vim.lsp.buf.implementation, bufopts)
+    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+    vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+    vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+    vim.keymap.set('n', '<space>wl', function()
+        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, bufopts)
+    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+    vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+    vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
+end
+
+local lsp_flags = {
+}
+
+
+-- Lua
+require'lspconfig'.lua_ls.setup {
+    on_attach = on_attach,
+    flags = lsp_flags,
+    settings = {
+        Lua = {
+            runtime = {
+                version = 'LuaJIT',
+            },
+            diagnostics = {
+                globals = {'vim'},
+            },
+            workspace = {
+                library = vim.api.nvim_get_runtime_file("", true),
+            },
+            telemetry = {
+                enable = false,
+            },
+        },
+    },
+}
+
+
+
+
+
+
 
 -----------------------------------------------------------
 -- Ctrl + P to invoke fzf file search
@@ -56,8 +125,4 @@ vim.api.nvim_set_keymap('n', '<c-P>',
 -----------------------------------------------------------
 -- lualine
 require('lualine_setup')
-lualine = require('lualine')
-lualine.setup({
-    options = { theme = 'iceberg_dark' }
-})
-lualine.setup()
+
