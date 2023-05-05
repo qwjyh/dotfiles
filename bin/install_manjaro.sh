b891c7123b19fa94d13d08b47bda17cd64cf4b5f
qwjyh <urataw421@gmail.com>
2023-05-05 09:22:44 +0900

    add: manjaro install wezterm config

--- bin/install_manjaro.sh
+++ bin/install_manjaro.sh
@@ -1,5 +1,8 @@
-#!/usr/bin/bash -euo
+#!/usr/bin/bash
+set -eu
 
 # alacritty config
-ln -sf (pwd)/dotfiles/alacritty.yml ~/.config/alacritty/alacritty.yml
+ln -sf $(pwd)/dotfiles/alacritty.yml ~/.config/alacritty/alacritty.yml
 
+mkdir -p ~/.config/wezterm
+ln -sf $(pwd)/dotfiles/wezterm/wezterm.lua ~/.config/wezterm/wezterm.lua
