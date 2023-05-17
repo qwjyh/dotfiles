f0070b4c1b05c80c3867d31948d790ef245a9e91
qwjyh <urataw421@gmail.com>
2023-05-17 13:02:12 +0900

    update: scoop list (add recyelc-bin)

--- bin/windows/scoop_apps/update_scoop_list.ps1
+++ bin/windows/scoop_apps/update_scoop_list.ps1
@@ -15,7 +15,7 @@ Write-Output (pwd).Path
 scoop export | Out-File .\bin\windows\scoop_apps\scoop_apps.json -Encoding utf8
 
 # create minimal JSON
-$minimal_list = @("7zip", "aria2", "bat", "fzf", "grep", "hexyl", "less", "python", "rclone", "rga", "scoop-completion", "shasum", "sudo", "tree-sitter", "ugrep")
+$minimal_list = @("7zip", "aria2", "bat", "fzf", "grep", "hexyl", "less", "python", "rclone", "recycle-bin", "rga", "scoop-completion", "shasum", "sudo", "tree-sitter", "ugrep")
 $parsed_json = Get-Content -Path .\bin\windows\scoop_apps\scoop_apps.json | ConvertFrom-Json
 $buckets = ($parsed_json | Select-Object buckets).buckets
 $apps = ($parsed_json | Select-Object apps).apps
