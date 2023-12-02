4c426c808919188b343dc0f35320d86ccb033cb6
qwjyh <urataw421@gmail.com>
2023-12-02 18:05:21 +0900

    new: nvim: telescope keybindings

--- dotfiles/neovim/init.lua
+++ dotfiles/neovim/init.lua
@@ -320,7 +320,18 @@ require('lualine_setup')
 
 -----------------------------------------------------------
 -- telescope
-vim.keymap.set('n', '<c-P>', function() require('telescope.builtin').find_files { sort_lastused = true } end) -- fd?
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<c-P>', function() builtin.find_files { sort_lastused = true } end) -- fd?
+vim.keymap.set('n', '<leader>ff', function() builtin.find_files { sort_lastused = true } end)
+vim.keymap.set('n', '<leader>fw', builtin.lsp_workspace_symbols)
+vim.keymap.set('n', '<leader>fd', builtin.lsp_document_symbols)
+vim.keymap.set('n', '<leader>fb', builtin.buffers)
+vim.keymap.set('n', '<leader>fg', builtin.live_grep)
+vim.keymap.set('n', '<leader>fh', builtin.help_tags)
+vim.keymap.set('n', '<leader>fc', builtin.git_commits)
+vim.keymap.set('n', '<leader>fgc', builtin.git_commits)
+vim.keymap.set('n', '<leader>fs', builtin.git_status)
+vim.keymap.set('n', '<leader>fgs', builtin.git_status)
 
 -----------------------------------------------------------
 -- Treesitter
