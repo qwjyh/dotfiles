be47ce7de7337153c560b4b939839f144e893015
qwjyh <urataw421@gmail.com>
2024-02-19 15:37:37 +0900

    update: nvim: add cssls

--- dotfiles/neovim/init.lua
+++ dotfiles/neovim/init.lua
@@ -637,7 +637,7 @@ lspconfig.typst_lsp.setup {
 }
 
 local lss = { "pyright", "rust_analyzer", "texlab", "ccls", "clangd", "tsserver", --[["tailwindcss"]] "hls", "cmake",
-    "csharp_ls", "html", "r_language_server", "ruff_lsp" }
+    "csharp_ls", "html", "r_language_server", "ruff_lsp", "cssls" }
 for _, ls in pairs(lss) do
     lspconfig[ls].setup {
         on_attach = on_attach,
