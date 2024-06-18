372040d1d77617f2fc14463f75c4ef71ee504737
qwjyh <urataw421@gmail.com>
2024-06-18 20:34:25 +0900

    nvim: update: setting up trouble.nvim v3
    - just using default configs

--- dotfiles/neovim/init.lua
+++ dotfiles/neovim/init.lua
@@ -131,7 +131,40 @@ require('lazy').setup({
     },
     {
         'folke/trouble.nvim',
-        -- config = function
+        opts = {},
+        cmd = "Trouble",
+        keys = {
+            {
+                "<space>xx",
+                "<cmd>Trouble diagnostics toggle<cr>",
+                desc = "Diagnostics (Trouble)",
+            },
+            {
+                "<space>xX",
+                "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+                desc = "Buffer Diagnostics (Trouble)",
+            },
+            {
+                "<space>cs",
+                "<cmd>Trouble symbols toggle focus=false<cr>",
+                desc = "Symbols (Trouble)",
+            },
+            {
+                "<space>cl",
+                "<cmd>Trouble lsp toggle focus=false win.position=bottom<cr>",
+                desc = "LSP Definitions / references / ... (Trouble)",
+            },
+            {
+                "<space>xL",
+                "<cmd>Trouble loclist toggle<cr>",
+                desc = "Location List (Trouble)",
+            },
+            {
+                "<space>xQ",
+                "<cmd>Trouble qflist toggle<cr>",
+                desc = "Quickfix List (Trouble)",
+            },
+        },
     },
     {
         'Julian/lean.nvim',
