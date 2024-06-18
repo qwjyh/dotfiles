4fefdd20b230a381d39d888aa13260cd157f15db
qwjyh <urataw421@gmail.com>
2024-06-18 19:49:36 +0900

    nvim: update: lazy installation for nvim 0.10

--- dotfiles/neovim/init.lua
+++ dotfiles/neovim/init.lua
@@ -1,7 +1,7 @@
 -----------------------------------------------------------
 -- Installing plugin manager 'lazy.nvim'
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
     vim.fn.system({
         "git",
         "clone",
