a26aa2cd02a1a6d2256e9bce0c0db473acc1aa85
qwjyh <urataw421@gmail.com>
2024-06-24 18:15:24 +0900

    nvim: update: use tinymist instead of typst-lsp

--- dotfiles/neovim/init.lua
+++ dotfiles/neovim/init.lua
@@ -642,7 +642,13 @@ lspconfig.powershell_es.setup {
 --     cmd = {'omnisharp'},
 -- }
 
-lspconfig.typst_lsp.setup {
+-- lspconfig.typst_lsp.setup {
+--     on_attach = on_attach,
+--     capabilities = capabilities,
+--     single_file_support = true,
+-- }
+
+lspconfig.tinymist.setup {
     on_attach = on_attach,
     capabilities = capabilities,
     single_file_support = true,
