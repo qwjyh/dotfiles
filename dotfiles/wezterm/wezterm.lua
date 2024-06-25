438823541a4e640207e4acc8b076e0c2ee21a7ab
qwjyh <urataw421@gmail.com>
2024-06-26 00:13:12 +0900

    wezterm: update: font family regarding juisee update v0.2.0

--- dotfiles/wezterm/wezterm.lua
+++ dotfiles/wezterm/wezterm.lua
@@ -51,7 +51,7 @@ return {
     -- font = wezterm.font 'FirgeNerd Console'
     font = wezterm.font_with_fallback {
         {
-            family = 'JuiseeHW Nerd Font',
+            family = 'Juisee HWNF',
             weight = 'Bold',
             harfbuzz_features = {
                 'calt=0', -- disables ligature
