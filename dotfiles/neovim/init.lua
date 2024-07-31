603c10544bdd4dc4310db328c64397b15c4dacb9
qwjyh <urataw421@gmail.com>
2024-07-31 14:09:31 +0900

    update: nvim: replace ruff lsp with ruff

--- dotfiles/neovim/init.lua
+++ dotfiles/neovim/init.lua
@@ -714,7 +714,7 @@ lspconfig.rust_analyzer.setup {
 }
 
 local lss = { "pyright", "texlab", "ccls", "clangd", "tsserver", --[["tailwindcss"]] "hls", "cmake",
-    "csharp_ls", "html", "r_language_server", "ruff_lsp", "cssls", "jsonls", "sqls" }
+    "csharp_ls", "html", "r_language_server", "cssls", "jsonls", "sqls", "vhdl_ls", "ruff" }
 for _, ls in pairs(lss) do
     lspconfig[ls].setup {
         on_attach = on_attach,
