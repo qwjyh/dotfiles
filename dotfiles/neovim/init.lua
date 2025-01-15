44be7093560924cf8d523ac0c5b6d910726f2e5b
qwjyh <urataw421@gmail.com>
2025-01-15 20:06:59 +0900

    update(nvim): add lemminx lsp config

--- dotfiles/neovim/init.lua
+++ dotfiles/neovim/init.lua
@@ -776,8 +776,8 @@ lspconfig.rust_analyzer.setup {
     }
 }
 
-local lss = { "pyright", "texlab", "ccls", "clangd", "ts_ls", --[["tailwindcss"]] "hls", "cmake",
-    "csharp_ls", "html", "r_language_server", "cssls", "jsonls", "sqls", "vhdl_ls", "ruff" }
+local lss = { "pyright", "texlab", --[[ "ccls", ]] "clangd", "ts_ls", --[["tailwindcss"]] "hls", "cmake",
+    "csharp_ls", "html", "r_language_server", "cssls", "jsonls", "sqls", "vhdl_ls", "ruff", "lemminx" }
 for _, ls in pairs(lss) do
     lspconfig[ls].setup {
         on_attach = on_attach,
