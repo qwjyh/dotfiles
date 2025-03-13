5f6e107b41c08b5faac368f2c09ccef7dd14816e
qwjyh <urataw421@gmail.com>
2025-03-14 04:34:52 +0900

    refactor(nvim): separate lsp on_attach func to module
    to share the same config between general lspconfig & lean config

--- dotfiles/neovim/init.lua
+++ dotfiles/neovim/init.lua
@@ -188,29 +188,7 @@ require('lazy').setup({
         -- see Julian/lean.nvim readme
         opts = {
             lsp = {
-                on_attach = function(client, bufnr)
-                    -- Enable completion triggered by <c-x><c-o>
-                    --vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-
-                    -- Mappings
-                    -- See `:help vim.lsp.*` for documentation on any of the below function
-                    local bufopts = { noremap = true, silent = true, buffer = bufnr }
-                    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-                    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-                    vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-                    vim.keymap.set('n', 'g1', vim.lsp.buf.implementation, bufopts)
-                    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
-                    vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
-                    vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
-                    vim.keymap.set('n', '<space>wl', function()
-                        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-                    end, bufopts)
-                    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
-                    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
-                    vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
-                    vim.keymap.set('n', 'grf', vim.lsp.buf.references, bufopts)
-                    vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
-                end,
+                on_attach = require("lsp_config").on_attach,
             },
             mappings = true,
         },
@@ -226,7 +204,6 @@ require('lazy').setup({
         },
         -- event = 'VeryLazy', -- doesn't work with existing comp and treesitter
         config = function()
-
             -- Setup orgmode
             require('orgmode').setup({
                 org_agenda_files = '~/orgfiles/**/*',
@@ -608,30 +585,7 @@ vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
 
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
-local on_attach = function(client, bufnr)
-    -- Enable completion triggered by <c-x><c-o>
-    --vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-
-    -- Mappings
-    -- See `:help vim.lsp.*` for documentation on any of the below function
-    local bufopts = { noremap = true, silent = true, buffer = bufnr }
-    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-    vim.keymap.set('n', 'gT', vim.lsp.buf.type_definition, bufopts)
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-    vim.keymap.set('n', 'g1', vim.lsp.buf.implementation, bufopts)
-    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
-    vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
-    vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
-    vim.keymap.set('n', '<space>wl', function()
-        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-    end, bufopts)
-    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
-    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
-    vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
-    vim.keymap.set('n', 'grf', vim.lsp.buf.references, bufopts)
-    vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
-end
+local on_attach = require("lsp_config").on_attach
 
 -- cmp_nvim_lsp supports additional LSP completion capabilities
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
