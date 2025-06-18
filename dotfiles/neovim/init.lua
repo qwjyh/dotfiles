25512b052b5c6c9e8101ddc51854c49c89e1f069
qwjyh <urataw421@gmail.com>
2025-06-18 15:19:41 +0900

    update(neovim): enable jetls sysimage

--- dotfiles/neovim/init.lua
+++ dotfiles/neovim/init.lua
@@ -635,10 +635,10 @@ lspconfig.julials.setup {
     end,
     capabilities = capabilities,
     cmd = { "julia", "--startup-file=no", "--history-file=no",
-        -- julials_so_option[1], julials_so_option[2],
+        julials_so_option[1], julials_so_option[2],
         -- use below 2 lines to collect script to be included in sysimage
-        -- '--trace-compile',
-        -- vim.env.HOME .. "/.julia/environments/nvim-lspconfig/tracecompile.jl",
+        '--trace-compile',
+        vim.env.HOME .. "/.julia/environments/nvim-lspconfig/tracecompile.jl",
         "-t4",
         "-e",
         [[
