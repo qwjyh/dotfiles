db09f7f951f6ba3d174db16f9f25bf1fa695059a
qwjyh <urataw421@gmail.com>
2025-09-06 16:03:13 +0900

    fix(fish): deprecated option -U of abbr

--- dotfiles/fish/abbr.fish
+++ dotfiles/fish/abbr.fish
@@ -1,4 +1,4 @@
-abbr -a -U -- l less
-abbr -a -U -- ll 'eza -la --icons --git'
+abbr --add -- l less
+abbr --add -- ll 'eza -la --icons --git'
 abbr --add -- qpv qpdfview --unique
 abbr --add -- lf yazi
