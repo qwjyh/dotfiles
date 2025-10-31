eaf0ddcdd47e7f198ad5f665fe354c373f622e73
qwjyh <urataw421@gmail.com>
2025-10-31 13:22:41 +0100

    misc(fish): format config.fish

--- dotfiles/fish/config.fish
+++ dotfiles/fish/config.fish
@@ -9,22 +9,19 @@ if status is-interactive
     set -gx my_fish_history "$HOME/my_fish_history.txt"
     function save_myhistory --on-event fish_prompt -d "Save custom shell log to $my_fish_history"
         set -l prev_status $status
-        echo "$(date '+%Y-%m-%d %H:%M:%S') $hostname:$fish_pid $PWD [$prev_status] $(history -1)" \
-            >> $my_fish_history
+        echo "$(date '+%Y-%m-%d %H:%M:%S') $hostname:$fish_pid $PWD [$prev_status] $(history -1)" >>$my_fish_history
     end
     # starting sign
-    echo "$(date '+%Y-%m-%d %H:%M:%S') $hostname:$fish_pid [START]" \
-        >> $my_fish_history
+    echo "$(date '+%Y-%m-%d %H:%M:%S') $hostname:$fish_pid [START]" >>$my_fish_history
     # exit sign
     function save_myhistory_exit --on-event fish_exit -d "Add exit sign to $my_fish_history"
-        echo "$(date '+%Y-%m-%d %H:%M:%S') $hostname:$fish_pid [EXIT]" \
-            >> $my_fish_history
+        echo "$(date '+%Y-%m-%d %H:%M:%S') $hostname:$fish_pid [EXIT]" >>$my_fish_history
     end
-    
+
     function set_win_title
-      echo -ne "\033]0; (basename "$PWD") \007"
+        echo -ne "\033]0; (basename "$PWD") \007"
     end
-    set starship_precmd_uesr_func "set_win_title"
+    set starship_precmd_uesr_func set_win_title
 
     # # keychain
     # set -x SHELL fish
@@ -56,12 +53,9 @@ if status is-interactive
                 --phony -q "$argv[-1]" \
                 --bind "change:reload:$RG_PREFIX {q}" \
                 --preview-window='50%:wrap'
-        ) && \
-        echo "opening $file" && \
-        open "$file"
+        ) && echo "opening $file" && open "$file"
     end
 end
-
 
 # opam
 # source ~/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
@@ -70,17 +64,13 @@ opam env --shell fish | source
 # key bindings
 bind \b backward-kill-word
 
-
 # env
 set -x LESS '-i -r -M -R -S -W -z-4 -x4'
-set -x EDITOR "/usr/bin/nvim"
+set -x EDITOR /usr/bin/nvim
 # julia sysimage
 set -x JL_SYSIMG_PATH "$HOME/dotfiles/julia-sysimages"
 set -x JL_SYSIMG_PLT "$HOME/dotfiles/julia-sysimages/sys-plotsmakie.so"
 set -x JL_SYSIMG_ETC "$HOME/dotfiles/julia-sysimages/sys-etc.so"
 
-
 # load local patch
 source ~/.config/fish/local_patch.fish
-
-
