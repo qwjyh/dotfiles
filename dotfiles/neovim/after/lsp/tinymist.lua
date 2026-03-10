3e75d30ffa1f4fcbb61107f8d9b992613ae43319
qwjyh <urataw421@gmail.com>
2026-03-10 21:40:41 +0900

    update(nvim): update tinymist config
    Some have migrated to tinymist-clientfeatures, and some are added from
    a project local config.

--- dotfiles/neovim/after/lsp/tinymist.lua
+++ dotfiles/neovim/after/lsp/tinymist.lua
@@ -2,28 +2,19 @@ vim.api.nvim_create_autocmd('LspAttach', {
     callback = function(ev)
         local client = vim.lsp.get_client_by_id(ev.data.client_id)
         if client and client.name == "tinymist" then
-            vim.api.nvim_buf_create_user_command(
-                ev.buf,
-                "TinymistShowCompileStatus",
-                require('tinymist_status').show_status_detail(ev.buf),
-                { desc = "Get compile status and words/character count", }
-            )
         end
     end
 })
 
+vim.keymap.set('n', '<leader>fo', require("telescope").extensions["tinymist-clientfeatures"].tinymist_outline,
+    { desc = "Show outline of tinymist preview" })
+
 ---@type vim.lsp.Config
 return {
     settings = {
-        compileStatus = 'enable',
         formatterMode = 'typstyle',
         ["lint.enabled"] = true,
-        preview = {
-            partialRendering = true,
-        }
     },
     handlers = {
-        ["tinymist/compileStatus"] = require("tinymist-clientfeatures.tinymist_status").compile_status_handler,
-        -- ["tinymist/documentOutline"] = outline_handler,
     }
 }
