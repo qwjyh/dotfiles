c539a65fa94e6daa534594f0a3afd5b76b667860
qwjyh <urataw421@gmail.com>
2026-05-09 23:46:28 +0900

    update(nvim): add jetls codelens reference count command handler
    https://aviatesk.github.io/JETLS.jl/release/#citeref-show_references_handler

--- dotfiles/neovim/after/lsp/jetls.lua
+++ dotfiles/neovim/after/lsp/jetls.lua
@@ -55,4 +55,20 @@ return {
             references = true,
         },
     },
+    commands = {
+        ["editor.action.showReferences"] = function(command, ctx)
+            local client = assert(vim.lsp.get_client_by_id(ctx.client_id))
+            local file_uri, position, references =
+                unpack(command.arguments)
+            local items = vim.lsp.util.locations_to_items(
+                references, client.offset_encoding)
+            vim.fn.setqflist({}, " ", {
+                title = command.title, items = items })
+            vim.lsp.util.show_document({
+                uri = file_uri,
+                range = { start = position, ["end"] = position },
+            }, client.offset_encoding)
+            vim.cmd("botright copen")
+        end,
+    },
 }
