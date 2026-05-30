32913c482d81e5533266506a399ce4a5e1ff68d4
qwjyh <urataw421@gmail.com>
2026-05-30 16:30:17 +0900

    update(nvim): remove unused jetls cmd for juliac compiled bin

--- dotfiles/neovim/after/lsp/jetls.lua
+++ dotfiles/neovim/after/lsp/jetls.lua
@@ -4,27 +4,6 @@
 ---https://github.com/aviatesk/JETLS.jl
 
 local root_files = { 'Project.toml', 'JuliaProject.toml' }
-local nvim_pid = vim.uv.os_getpid()
-
----Run juliac compiled executable of jetls without bundling.
----@param julia_path string
----@param jetls_path string
----@param jetls_opts string[]?
----@return { cmd: string[], cmd_env: table }
-local function run_executable(julia_path, jetls_path, jetls_opts)
-    local default_cmd = {
-        jetls_path,
-    }
-    vim.list_extend(default_cmd, { '--clientProcessId=' .. vim.uv.os_getpid() })
-    vim.list_extend(default_cmd, jetls_opts or {})
-
-    local cmd_env = {
-        LD_LIBRARY_PATH = table.concat({ julia_path .. "/lib/", julia_path .. "/lib/julia/" }, ':'),
-        JULIA_NUM_THREADS = '12,2',
-    }
-
-    return { cmd = default_cmd, cmd_env = cmd_env }
-end
 
 local default_cmd = {
     'jetls',
@@ -34,11 +13,6 @@ local default_cmd = {
     -- '--clientProcessId=' .. nvim_pid,
 }
 
----@diagnostic disable-next-line: unused-local
-local jetls_compiled_opts = run_executable(
-    vim.uv.os_homedir() .. '/.julia/juliaup/julia-1.12.2+0.x64.linux.gnu/',
-    '/home/qwjyh/work/julia/JETLS.jl/jetls'
-)
 
 ---@type vim.lsp.Config
 return {
